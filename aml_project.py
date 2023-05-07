a4116567e478cbfc7154c4e121b7f07724b3d394
Yashwant Singh Parihar <Yashwantsingh.sngh@gmail.com>
2023-05-07 19:18:18 +0530

    Remove redundant code

--- aml_project.py
+++ aml_project.py
@@ -91,42 +91,6 @@ class ResidualConvBlock(nn.Module):
             x2 = self.conv2(x1)
             return x2
 
-class ResidualConvBlock(nn.Module):
-    def __init__(
-        self, in_channels: int, out_channels: int, is_res: bool = False
-    ) -> None:
-        super().__init__()
-        '''
-        standard ResNet style convolutional block
-        '''
-        self.same_channels = in_channels==out_channels
-        self.is_res = is_res
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
-            nn.BatchNorm2d(out_channels),
-            nn.GELU(),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
-            nn.BatchNorm2d(out_channels),
-            nn.GELU(),
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if self.is_res:
-            x1 = self.conv1(x)
-            x2 = self.conv2(x1)
-            # this adds on correct residual in case channels have increased
-            if self.same_channels:
-                out = x + x2
-            else:
-                out = x1 + x2 
-            return out / 1.414
-        else:
-            x1 = self.conv1(x)
-            x2 = self.conv2(x1)
-            return x2
-
 
 class UnetDown(nn.Module):
     def __init__(self, in_channels, out_channels):
